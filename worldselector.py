d4ab8f28aaee027d2b9516126404dc4e51c5b08c
Sean Mizen <sean@seanmizen.com>
2021-12-07 14:17:34 +0000

    Added WorldBusy

--- worldselector.py
+++ worldselector.py
@@ -1,8 +1,9 @@
 import Worlds.world1 as world
+import Worlds.world1busy as worldbusy
 import Worlds.world1rushhour as world1rushhour
 import Worlds.world1gridlock as world1gridlock
 import Worlds.probtestworld as probtestworld
 
 
 def export():
-    return world.export()
+    return worldbusy.export()
